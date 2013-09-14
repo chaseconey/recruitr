46f7c6f7651e8332f44dfef3325abccb016644e3
Chase Coney <chasiepoo@gmail.com>
2013-09-14 17:31:39 -0500

    Project up and running

---
+++ app/database/seeds/UserTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+class UserTableSeeder extends Seeder
+{
+
+    public function run()
+    {
+        DB::table('users')->delete();
+        User::create(
+            array(
+                'name'     => 'Chase Coney',
+                'email'    => 'chase.coney@gmail.com',
+                'password' => Hash::make('rawr'),
+            )
+        );
+    }
+
+}
